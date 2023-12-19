87870d09ab3a33e3c4764df5d36cf5706bffcd86
idrecun <idrecun@gmail.com>
2023-12-19 16:43:34 +0100

    Desperate attempt

--- gointerfaces/test_util.go
+++
@@ -1,4 +0,0 @@
-package gointerfaces
-
-//go:generate moq -stub -out ./zkevmsentry/mocks.go ./zkevmsentry SentryServer SentryClient
-//go:generate moq -stub -out ./zkevmremote/mocks.go ./zkevmremote KVClient KV_StateChangesClient
