479f38e6b72c5c4eb9d765243db081e952dec097
pdrobnjak <drobnjakpavle95@gmail.com>
2024-05-17 16:58:18 +0200

    Fix sort predicate

--- gointerfaces/remote/sort.go
+++ gointerfaces/remote/sort.go
@@ -6,9 +6,9 @@ import (
 	"github.com/tenderly/zkevm-erigon-lib/gointerfaces/types"
 )
 
-func NodeInfoReplyLess(i, j *types.NodeInfoReply) bool {
+func NodeInfoReplyLess(i, j *types.NodeInfoReply) int {
 	if cmp := strings.Compare(i.Name, j.Name); cmp != 0 {
-		return cmp == -1
+		return cmp
 	}
-	return strings.Compare(i.Enode, j.Enode) == -1
+	return strings.Compare(i.Enode, j.Enode)
 }
